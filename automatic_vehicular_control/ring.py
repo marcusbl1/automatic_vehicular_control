47088770caaf6e6095a07e1a2ed361a4e3386ebe
jessding <jessding@mit.edu>
2023-12-05 14:07:25 -0500

    adding TTC for ring

--- automatic_vehicular_control/ring.py
+++ automatic_vehicular_control/ring.py
@@ -155,6 +155,9 @@ class RingEnv(Env):
         reward = np.mean([v.speed for v in (ts.vehicles if c.global_reward else rl_type.vehicles)])
         if c.accel_penalty and hasattr(self, 'last_speed'):
             reward -= c.accel_penalty * np.abs(rl.speed - self.last_speed) / c.sim_step
+        #add safety reward structure
+        lam = 1 # move to cfg
+        reward += lam * ts.tc.vehicle.getParameter(id, "device.ssm.minTTC")
         self.last_speed = rl.speed
 
         return obs.astype(np.float32), reward, False, None
